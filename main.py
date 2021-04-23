5c64fd926505ea9b31e4ee755c157cba3a83ab85
Andreas Kasten <akasten@users.noreply.github.com>
2021-04-23 12:16:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-#!/usr/bin/env python3
-print("Hello World.")
+#!/usr/bin/env python2
+import traceback
+
+def do_computation():
+    raise Exception("Secret info")
+
+def __init__():
+    try:
+        do_computation()
+    except Exception as e:
+        return traceback.format_exc()
+
+x = __init__()
+print x
